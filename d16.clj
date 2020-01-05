3abbac5100d83f8f72e3ff5dd20e348ee06c3b02
dmitry kim <jason@nichego.net>
2020-01-06 00:29:47 +0300

    + d16 (duh, that was actually pretty easy)

--- d16.clj
+++ d16.clj
@@ -21,12 +21,36 @@
 
 (defn one [] (run (slurp "16.in") 100))
 
-(let [n 39]
+(def test2-1 "03036732577212944063491565474664")
+
+(defn halve [xs] (let [v (vec xs)] (subvec v (/ (count v) 2))))
+
+(defn run2-1 [xs]
+  (->> xs
+       reverse
+       (reductions +)
+       (map #(mod % 10))
+       reverse
+       vec))
+
+(defn run2-n [arg n] (nth (iterate run2-1 arg) n))
+
+(defn repeat-vec [v n]
+  (apply vector (take (* n (count v)) (cycle v))))
+
+(defn run2 [s n]
+  (let [tail (halve (repeat-vec (parse s) 10000))
+        pos (- (Integer. (subs s 0 7)) (count tail))
+        tail' (run2-n tail n)
+        rv (subvec tail' pos (+ pos 8))]
+    (apply str rv)))
+
+'(let [n 39]
   (dotimes [i n]
     (->> i seq-n (take n) (map {-1 "-" 1 "+" 0 0}) println)))
 
 (defn two []
-  "not implemented")
+  (time (run2 (slurp "16.in") 100)))
 
 (defn -main [& args]
   (println "1." (one)) ; 50879490 too low
@@ -38,4 +62,8 @@
     (is (= (run "80871224585914546619083218645595" 100) "24176176"))
     (is (= (run "19617804207202209144916044189917" 100) "73745418"))
     (is (= (run "69317163492948606335995924319873" 100) "52432133"))
+    )
+  (testing "b-tests"
+    (is (= (run2 "03036732577212944063491565474664" 100) "84462026"))
+    (is (= (run2 "02935109699940807407585447034323" 100) "78725270"))
     ))
