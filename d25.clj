b03858e50e5b8314d69dd33e12d1cee10e782b1d
dmitry kim <jason@nichego.net>
2020-01-07 15:39:45 +0300

    * d25: WIP

--- d25.clj
+++ d25.clj
@@ -8,34 +8,74 @@
 
 (defn read-input [] (read-string (str "[" (slurp "25.in") "]")))
 
+(defn translate-cmd [l]
+  (case l
+    "a" "west"
+    "d" "east"
+    "s" "south"
+    "w" "north"
+    "i" "inv"
+    ">" (do
+          (print "Filename: ")
+          (flush)
+          (slurp (read-line)))
+    l))
+
+(defn line-xform [rf]
+  (let [line (volatile! [])]
+    (fn
+      ([] (rf))
+      ([result]
+       (let [l @line]
+         (vreset! line [])
+         (cond-> result
+           (seq l) (rf (apply str l))
+           true rf)))
+      ([result c]
+       (let [c (char c)]
+         (if (= c \newline)
+           (let [l @line]
+             (vreset! line [])
+             (rf result (apply str l)))
+           (vswap! line conj c)))))))
+
+(defn lines-xform [rf]
+  (let [lines (volatile! [])]
+    (fn
+      ([] (rf))
+      ([result]
+       (let [ls @lines]
+         (vreset! lines [])
+         (cond-> result
+           (seq ls) (rf ls)
+           true rf)))
+      ([result l]
+       (vswap! lines conj l)
+       (when (or (> (count @lines) 100)
+               (re-matches #"^Command\?$" l))
+         (rf result @lines)
+         (vreset! lines []))))))
+
 (defn runner []
   (let [code (read-input)
-        in (chan) out (chan)]
+        in (chan)
+        out (chan 1 (comp line-xform lines-xform))]
+
     (vm/run (vm/create code) in out)
+
     (go (loop []
-          (let [c (char (<! out))]
-            (when c
-              (print c)
-              (when (= \newline c) (flush))
-              (recur)))))
+          (if-let [ls (<! out)]
+            (do
+              (doseq [l ls] (println l))
+              (recur))
+            (println ">>>>>>>>>>>>> VM HALT <<<<<<<<<<<<<<"))))
+
     (loop []
       (when-let [l (read-line)]
-        (let [l
-              (case l
-                "a" "west"
-                "d" "east"
-                "s" "south"
-                "w" "north"
-                "i" "inv"
-                ">" (do
-                      (print "Filename: ")
-                      (flush)
-                      (slurp (read-line)))
-                l)]
-          (doseq [c l] (>!! in (int c)))
-          (>!! in (int \newline))
-          (recur))))
-    ))
+        (doseq [c (translate-cmd l)] (>!! in (int c)))
+        (>!! in (int \newline))
+        (recur))))
+    )
 
 (defn one [] (runner))
 
